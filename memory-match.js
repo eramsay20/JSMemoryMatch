d9631ea5ac1d7b5e1226462fab463962dbdd147d
Eric Ramsay <64610054+eramsay20@users.noreply.github.com>
2020-05-28 15:52:51 -0400

    completed frame, able to randomly generate a specified number of cards (factor of 4) and have images pairs automatically generated and shuffled

--- memory-match.js
+++ memory-match.js
@@ -28,45 +28,140 @@ Stretch Requirements:
 3. The game is timed, with the player’s final time displayed at the end 
 -----------------------------------------------------------------------*/
 
+// RESOURCE: https://scotch.io/tutorials/how-to-build-a-memory-matching-game-in-javascript // 
+
 //----------------------Random Number Generator (START)-------------------------------/
 function randNum (numInput) {
-    let num = Math.floor(Math.random() * (numInput+1));
-    console.log(num);
+    let num = Math.floor(Math.random() * (numInput));
+    // console.log(num);
     return num;
 };
 //----------------------Random Number Generator (END)-------------------------------/
 
 //----------------------Identify Objects & Parents (START)-------------------------------/
-// document.querySelector('body').addEventListener('click', (event) => {
-//     let clickTarget = event.target;
-//     let clickParent = clickTarget.parentNode;
-//      console.log(clickTarget);
-//      console.log(clickParent);
-// });
+document.querySelector('body').addEventListener('click', (event) => {
+    let clickTarget = event.target;
+    let clickParent = clickTarget.parentNode;
+    //  console.log(clickTarget);
+    //  console.log(clickParent);
+});
 //----------------------Identify Objects & Parents (END)-------------------------------/
 
-//----------------------Generate Cards Array for Game (START)-------------------------------/
+//----------------------STORE IMAGES IN ARRAY (START)-------------------------------/
+
+let imageArr = [];
+
+    let img0 ="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/01_of_spades_A.svg/800px-01_of_spades_A.svg.png";
+    imageArr.push(img0);
+
+    let img1 ="https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/926/4H__83243.1440113515.1280.1280.png?c=2";
+    imageArr.push(img1);
+
+    let img2 = "https://theplayingcardfactory.com/wp-content/uploads/revslider/rev1/4cards.png";
+    imageArr.push(img2);
+
+    let img3 ="https://www.tripleclicks.com/images/site/games/cardking/i-kings.png";
+    imageArr.push(img3);
+
+console.log(imageArr);
+// https://stackoverflow.com/questions/35397728/storing-images-in-javascript-variables
+//----------------------STORE IMAGES IN ARRAY (END)-------------------------------/
 
 // console.log(document.querySelectorAll("#card")); // WORKS, CREATES ARRAY
 
 document.querySelector('.startButton').addEventListener('click', () => {
     let cardArr = document.querySelectorAll('#card');
-    let cardCount = parseFloat(document.querySelector('.inputCardNum').value);
+    let initialCardCount = parseFloat(document.querySelector('.inputCardNum').value);
     let cardTable = document.querySelector('.cardTable');
 
-        console.log(cardArr);
-        console.log(cardArr.length);
-        console.log(cardCount);
-        console.log(cardTable);
+    let maxImgCount = (initialCardCount/4); // since there are 4 types of images
+    let img0Count = 0;
+    let img1Count = 0;
+    let img2Count = 0;
+    let img3Count = 0;
+
+        // console.log(cardArr);
+        // console.log(initialCardCount);
+        // console.log(cardTable.length);
+        // console.log(cardTable);
 
 
-    for (i=cardArr.length; i<cardCount; i+=1){
-        let newCard = document.createElement('div');
-        newCard.className = "cardBack";
-        cardTable.appendChild(newCard);
-    }
+    for (i=0; i < initialCardCount; i+=1){
+        let cardImageNum = randNum(4);
+        // console.log(cardImageNum);
 
+        if (cardImageNum == 0){
+            if(img0Count < maxImgCount){
+                let newCard = document.createElement('div');
+                let newCardImage = document.createElement('img');
 
+                newCard.className = "cardBack";
+                newCardImage.className = "cardImage";
+                newCardImage.src = imageArr[0];
+                console.log(newCardImage.src);
+
+                newCard.appendChild(newCardImage);
+                cardTable.appendChild(newCard);
+                
+                img0Count++;}
+            else {
+                initialCardCount = initialCardCount+1;
+            }
+        }
+        else if (cardImageNum == 1){
+            if(img1Count < maxImgCount){
+                let newCard = document.createElement('div');
+                let newCardImage = document.createElement('img');
+
+                newCard.className = "cardBack";
+                newCardImage.className = "cardImage";
+                newCardImage.src = imageArr[1];
+
+                newCard.appendChild(newCardImage);
+                cardTable.appendChild(newCard);
+                img1Count++;}
+            else {
+                initialCardCount = initialCardCount+1;
+            }
+        }
+        else if (cardImageNum == 2){
+            if(img2Count < maxImgCount){
+                let newCard = document.createElement('div');
+                let newCardImage = document.createElement('img');
+
+                newCard.className = "cardBack";
+                newCardImage.className = "cardImage";
+                newCardImage.src = imageArr[2];
+
+                newCard.appendChild(newCardImage);
+                cardTable.appendChild(newCard);
+                img2Count++;}
+            else {
+                initialCardCount = initialCardCount+1;
+            }
+        }
+        else if (cardImageNum == 3){
+            if(img3Count < maxImgCount){
+                let newCard = document.createElement('div');
+                let newCardImage = document.createElement('img');
+
+                newCard.className = "cardBack";
+                newCardImage.className = "cardImage";
+                newCardImage.src = imageArr["3"];
+
+                newCard.appendChild(newCardImage);
+                cardTable.appendChild(newCard);
+                img3Count++;}
+            else {
+                initialCardCount = initialCardCount+1;
+            }
+        }
+        else {
+           break;
+        }
+                       
+    };
+    // console.log(Object.keys(cardTable).length);
 
     
     // let newCard = document.createElement('li');
